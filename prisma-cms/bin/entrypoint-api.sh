6b753305a776f27601448ef5d64cbd23fa3d637c
lanets <n.lanets@well-connect.net>
2019-12-04 09:20:22 +0300

    1.1.0-beta

---
+++ prisma-cms/bin/entrypoint-api.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+until curl --silent $endpoint >/dev/null; do
+  >&2 echo "Prisma server is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Prisma server is up - executing command"
+
+# Проверяем наличие схемы
+if [ -f "src/schema/generated/api.fragments.js" ]; then 
+  echo 'Skip deploy schema due already deployed'
+else
+  echo 'Deploy prisma schema'
+  yarn deploy
+fi
+
+
+# if $cmd; then
+#   exec $cmd
+# fi
+  
+# Есть небольшая вероятность, что если использовать не $@, а собственную переменную, то 
+# деплой контейнера в таком случае заменит имеющийся контейнер сразу.
+# Но это не точно.
+# https://stackoverflow.com/questions/55173962/docker-compose-entrypoint-script-stops-container-with-exit-0
+# exec $cmd
+exec "$@"
---
+++ prisma-cms/bin/entrypoint-front.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# Проверяем наличие схемы
+# Пока ее не будет, дальнейшее выполнение нельзя делать
+while [ ! -f "src/schema/generated/api.fragments.js" ]; do
+  >&2 echo "Schema still not deployed from api service - sleeping"
+  sleep 5
+done
+
+>&2 echo "Deployed schema exists - executing command"
+
+
+
+ls -la
+
+if [ $ENV_MODE = "production" ] ; then
+
+  yarn build;
+
+fi
+
+# if $cmd; then
+#   exec $cmd
+# fi
+  
+# Есть небольшая вероятность, что если использовать не $@, а собственную переменную, то 
+# деплой контейнера в таком случае заменит имеющийся контейнер сразу.
+# Но это не точно.
+# https://stackoverflow.com/questions/55173962/docker-compose-entrypoint-script-stops-container-with-exit-0
+# exec $cmd
+exec "$@"
