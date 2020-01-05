ff6f3243acd25d5612dbed79551bfd3d69911eb2
Fi1osof <n.lanets@modxclub.ru>
2020-01-05 06:17:03 +0300

    Fix deploy schema

--- prisma-cms/bin/front.sh
+++ prisma-cms/bin/front.sh
@@ -2,14 +2,17 @@
 
 set -e
 
+# Деплоим призма-схему
+deploy-api-schema.sh
+
 # Проверяем наличие схемы
 # Пока ее не будет, дальнейшее выполнение нельзя делать
-while [ ! -f "src/schema/generated/api.fragments.js" ]; do
-  >&2 echo "Schema still not deployed from api service - sleeping"
-  sleep 5
-done
+# while [ ! -f "src/schema/generated/api.fragments.js" ]; do
+#   >&2 echo "Schema still not deployed from api service - sleeping"
+#   sleep 5
+# done
 
->&2 echo "Deployed schema exists - executing command"
+# >&2 echo "Deployed schema exists - executing command"
 
 if [ $ENV_MODE = "production" ] ; then
 
